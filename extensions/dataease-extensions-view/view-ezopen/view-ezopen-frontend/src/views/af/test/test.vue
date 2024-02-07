c6d4b711fe7d71b1922829e6cf536e14e651dd58
quanminzhe <2602671329@qq.com>
2024-02-07 11:55:29 +0800

    ezopen 测试vue组件

---
+++ extensions/dataease-extensions-view/view-ezopen/view-ezopen-frontend/src/views/af/test/test.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+  <iframe
+    :src=url
+    width="600"
+    height="400"
+    id="ysOpenDevice"
+    allowfullscreen
+  >
+  </iframe>
+</div>
+</template>
+
+<script>
+export default {
+  name: "test",
+  data() {
+    return {
+      url: "https://open.ys7.com/console/jssdk/pc.html?accessToken=at.5iydnanc41jre3u983hwxle839nxbj89-1e8bcec4ad-0i102yc-ewajt1pw9&url=ezopen://open.ys7.com/J96844961/1.hd.live"
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
